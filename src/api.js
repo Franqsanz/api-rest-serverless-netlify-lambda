3a3e8e83c01b1e13751c46c1433e902293c88911
franco sanchez <franco_genta@hotmail.com>
2020-08-28 13:35:56 -0300

    update api

--- src/api.js
+++ src/api.js
@@ -7,21 +7,53 @@ const router = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-router.get('/', (req, res) => res.send('Express with Netlify Lambda'));
-
-const fruit = {
-    "Fruit": [
-        "Banana",
-        "Apple",
-        "Lemon",
-        "Peach",
-        "Grapefruit",
-        "Plum"
+// Emulation Data Base
+const fruits = {
+    Fruits: [
+        {
+            id: 1,
+            Fruit: "Banana",
+            Climate: "Damp"
+        },
+        {
+            id: 2,
+            Fruit: "Apple",
+            Climate: "Cold"
+        },
+        {
+            id: 3,
+            Fruit: "Lemon",
+            Climate: "Semi Tropical"
+        }
     ]
 }
 
-router.get('/fruit', (req, res) => res.send(fruit));
+router.get('/', (req, res) => res.send(`
+    <section>
+        <h1>Express with Netlify Lambda</h1>
+        <a href="http://localhost:9000/api/fruits">
+            http://localhost:9000/api/fruits
+        </a>
+    </section>`
+));
+router.get('/fruits', (req, res) => res.send(fruits));
+router.get('/fruits/:id', (req, res) => {
+    const id = req.params.id;
 
-app.use('/api', router)
+    const search = fruits.Fruits.find((fruit) => fruit.id == id);
+    res.send(search);
+});
+router.post('/fruits/', (req, res) => {
+    const newFruit = {
+        id: 4,
+        Fruit: "Pear",
+        Climate: "Warm and Humid"
+    }
+
+    fruits.Fruits.push(newFruit);
+    res.send(fruits);
+});
+
+app.use('/api', router);
 
 module.exports.handler = serverless(app);
